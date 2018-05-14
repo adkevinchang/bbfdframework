9ca438988bc14232081fa39e6e0e442ed5023108
kevin <kevinchang_001@126.com>
2018-05-14 19:58:22 +0800

    bbfd
    bbfd1.0

---
+++ ModelBase.lua
@@ -0,0 +1,45 @@
+--region ModelBase.lua
+--Date
+--此文件由[BabeLua]插件自动生成
+--处理用户本地数据与远程数据，并与逻辑类交互
+--kevin
+
+local ModelBase = class("ModelBase");
+
+function ModelBase:ctor(vo)
+    self.vo_ = vo;
+    if self.onCreate then self:onCreate() end
+end
+
+--静态数据staticData  动态数据vo的处理使用全局cc.exports.xxx = {}
+function ModelBase:onCreate()
+   
+end
+
+--清除所有的内存和监听事件，置空静态数据
+function ModelBase:goDispose()
+
+end
+
+
+--endregion
+
+
+--region 属性管理
+
+function ModelBase:evtMgr()
+     return bbfd.evtMgr
+end
+
+function ModelBase:netMgr()
+     return bbfd.netMgr
+end
+
+--当前模块功能的动态数据类
+function ModelBase:getVo()
+    return self.vo_
+end
+
+--endregion
+
+return ModelBase;
---
+++ init.lua
@@ -0,0 +1,10 @@
+
+local _M = {}
+
+_M.AppBase  = import(".AppBase")
+_M.ViewBase = import(".ViewBase")
+_M.ControlBase  = import(".ControlBase")
+_M.ModelBase = import(".ModelBase")
+_M.ModuleBase = import(".ModuleBase")
+
+return _M
